085c92795fd0dd290683ef53365e5a84c020efac
juampyz7 <jlojaquichimbo@gmail.com>
2024-10-24 12:54:21 -0500

    solucion del cuarto ejercicio

--- Proyecto Taller 3/src/Ejercicio4.java
+++ Proyecto Taller 3/src/Ejercicio4.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario iTC
- */
-public class Ejercicio4 {
+public class Ejercicio4 { 
+    public static void main(String[] args) {
+        boolean respuesta;
+        respuesta = ((20)/4) + 2>=3;
+        System.out.println("respuesta = " + respuesta);
+    }
     
 }
