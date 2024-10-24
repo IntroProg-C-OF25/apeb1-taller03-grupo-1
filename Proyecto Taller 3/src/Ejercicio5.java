750ffdd4a740aa1e27104c6a561722e7f36435fc
juampyz7 <jlojaquichimbo@gmail.com>
2024-10-24 12:57:41 -0500

    solucion del quinto ejercicio

--- Proyecto Taller 3/src/Ejercicio5.java
+++ Proyecto Taller 3/src/Ejercicio5.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario iTC
- */
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = (Math.pow(2,2) + 12 - 1)/3;
+        System.out.println("respuesta  = " + respuesta);
+    }
     
 }
