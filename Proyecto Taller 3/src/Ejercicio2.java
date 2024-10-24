2709bb99504f9fe7d48eb8c8b8a8fb364d8f3b38
juampyz7 <jlojaquichimbo@gmail.com>
2024-10-24 12:30:47 -0500

    solucion del segundo ejercicio

--- Proyecto Taller 3/src/Ejercicio2.java
+++ Proyecto Taller 3/src/Ejercicio2.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario iTC
- */
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = Math.pow(10,2) + 10;
+        System.out.println("respuesta = " + respuesta);
+    }
     
 }
