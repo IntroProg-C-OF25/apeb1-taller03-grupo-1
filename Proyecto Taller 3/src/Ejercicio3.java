7a9b5dc33b4ff88ae0543eea50a957ff3a892645
juampyz7 <jlojaquichimbo@gmail.com>
2024-10-24 12:39:39 -0500

    solucion del tercer ejercicio

--- Proyecto Taller 3/src/Ejercicio3.java
+++ Proyecto Taller 3/src/Ejercicio3.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+public class Ejercicio3 { 
+    public static void main(String[] args) {
+        double respuesta;
+        respuesta = Math.pow(10,2) + Math.sqrt(16) + ((20)/1+11);
+        System.out.print("respuesta = " + respuesta);
+    }
 
-/**
- *
- * @author Usuario iTC
- */
-public class Ejercicio3 {
-    
+
 }
