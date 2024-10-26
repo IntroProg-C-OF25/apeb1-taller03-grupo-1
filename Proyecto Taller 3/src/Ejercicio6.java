18e2b8309d77b309d07bcc6b9986b9c4099230c2
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-10-26 09:12:11 -0500

    solucion del ejercicio 6

--- Proyecto Taller 3/src/Ejercicio6.java
+++ Proyecto Taller 3/src/Ejercicio6.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario iTC
- */
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+        System.out.println(((Math.sqrt(81) + 9) / 3)==9 && (10>1));
+    } 
 }
