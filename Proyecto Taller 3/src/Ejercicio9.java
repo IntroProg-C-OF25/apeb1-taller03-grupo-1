b20457b53331ca908885b858689a235f755c85d7
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-10-26 09:17:57 -0500

    solucion del ejercicio 9

--- Proyecto Taller 3/src/Ejercicio9.java
+++ Proyecto Taller 3/src/Ejercicio9.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario iTC
- */
 public class Ejercicio9 {
+    public static void main(String[] args) {
+        double a = Math.sqrt(81);
+        double b = Math.sqrt(25) * 10;
+        double resultado = a + b;
+        System.out.println(resultado >= 50);
+    }
     
 }
